1df24b409a9edcf68f4beb0972c3a7163db60ca3
bazuin-32 <ameenpiano@gmail.com>
2022-07-21 21:38:10 -0600

    Fix image layout issues

--- startpage/static/main.js
+++ startpage/static/main.js
@@ -1,6 +1,9 @@
 // get a random background
 const body = document.body;
-body.style.background = "url('https://picsum.photos/1920/1080?random=1') no-repeat center center fixed";
+
+// these image dimensions are 1.2 * 1920x1080, since having the image
+// aspect ratio not wide enough to fit the screen causes problems
+body.style.background = "url('https://picsum.photos/2304/1296?random=1') no-repeat center center fixed";
 
 document.getElementById("main-input").addEventListener("keyup", (event) => {
 	if (event.key !== "Enter") return;
