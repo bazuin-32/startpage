1b5cec9d0818abd1a597fdc33215f89f00aeff76
bazuin-32 <ameenpiano@gmail.com>
2022-07-22 17:34:35 -0600

    Make image size set to max screen size, and remove unnecessary js

--- startpage/static/main.js
+++ startpage/static/main.js
@@ -1,14 +1,7 @@
 // get a random background
 const body = document.body;
 
-// these image dimensions are 1.2 * 1920x1080, since having the image
-// aspect ratio not wide enough to fit the screen causes problems
-body.style.background = "url('https://picsum.photos/2304/1296?random=1') no-repeat center center fixed";
-
-document.getElementById("main-input").addEventListener("keyup", (event) => {
-	if (event.key !== "Enter") return;
-	event.preventDefault();
-
-	let request = new XMLHttpRequest();
-	request.open("POST", "/go/");
-});
+// get the window size
+const width = window.innerWidth || document.documentElement.clientWidth || body.clientWidth;
+const height = window.innerHeight || document.documentElement.clientHeight || body.clientHeight;
+body.style.background = `url('https://picsum.photos/${width}/${height}?random=1') no-repeat center center fixed`;
