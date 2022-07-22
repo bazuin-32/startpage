6a5e1d603443ee9ebb337e420e588e8fc69084dd
bazuin-32 <ameenpiano@gmail.com>
2022-07-21 18:50:33 -0600

    Added functionality and general improvements

--- startpage/startpage.py
+++ startpage/startpage.py
@@ -6,6 +6,8 @@ from werkzeug.exceptions import abort
 from startpage.auth import login_required
 from startpage.db import get_db
 
+import re
+
 bp = Blueprint('startpage', __name__)
 
 
@@ -23,3 +25,32 @@ def index():
 	).fetchall()
 
 	return render_template('startpage/index.html', history=history, freq_visited=freq_visited)
+
+
+@bp.route('/go', methods=('POST',))
+@login_required
+def go():
+	query = request.form['query']
+	
+	# add the query to the history db
+	db = get_db()
+
+	db.execute(
+		"INSERT INTO history (user_id, query) VALUES (?, ?)",
+		(g.user['id'], query)
+	)
+
+	db.commit()
+
+	# check if protocol was provided, or
+	# if the query is a valid domain or ip address
+	if re.match(r'^(http|https)://', query):
+		return redirect(query)
+	elif re.match(r'^(www\.)?[a-zA-Z0-9\-]+\.[a-zA-Z]{2,}$', query):
+		return redirect('https://' + query)
+	elif re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', query):
+		return redirect('https://' + query)
+
+	# if the query is not a valid domain or ip address,
+	# interpret it as a search query
+	return redirect(f"https://www.google.com/search?q={query}")
--- startpage/static/main.js
+++ startpage/static/main.js
@@ -1,3 +1,11 @@
 // get a random background
 const body = document.body;
 body.style.background = "url('https://picsum.photos/1920/1080?random=1') no-repeat center center fixed";
+
+document.getElementById("main-input").addEventListener("keyup", (event) => {
+	if (event.key !== "Enter") return;
+	event.preventDefault();
+
+	let request = new XMLHttpRequest();
+	request.open("POST", "/go/");
+});
